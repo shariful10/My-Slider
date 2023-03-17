3d3fa79cef40b3cfd3ebe647035e1d8538553ebe
Sk Shariful Islam <skshariful10@gmail.com>
2023-03-17 16:19:27 +0600

    Slider Description Set Dinamically

--- js/slider.js
+++ js/slider.js
@@ -22,17 +22,32 @@ const titles = [
     'Ninth Slide Label'
 ]
 
+const description = [
+    'This is my content for the first slide.',
+    'This is my content for the second slide.',
+    'This is my content for the third slide.',
+    'This is my content for the forth slide.',
+    'This is my content for the fifth slide.',
+    'This is my content for the sixth slide.',
+    'This is my content for the seventh slide.',
+    'This is my content for the eighth slide.',
+    'This is my content for the ninth slide.'
+]
+
 let imgIndex = 0;
 const imgEl = document.getElementById("slider-img");
 const titleEl = document.getElementById("slider-title");
+const descEl = document.getElementById("slider-deccription");
 
 setInterval(() => {
-    if ((imgIndex === images.length) && (imgIndex === titles.length)) {
+    if ((imgIndex === images.length) && (imgIndex === titles.length) && (imgIndex === description.length)) {
         imgIndex = 0;
     }
     const imgUrl = images[imgIndex];
-    imgEl.setAttribute("src", imgUrl);
     const titleUrl = titles[imgIndex];
+    const descUrl = description[imgIndex];
+    imgEl.setAttribute("src", imgUrl);
     titleEl.innerText = titleUrl;
+    descEl.innerText = descUrl;
     imgIndex++;
 }, 2000)
